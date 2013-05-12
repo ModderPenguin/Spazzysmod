4a54bf9f2058f93314ab51cbafb97557e9a05471
ModderPenguin <modderpenguin@gmail.com>
2013-05-12 16:19:27 +1000

    Made some changes to the moon
    The moon now only spawns Gophers… Yeah, Gophers

--- Spazzysmod/spazzysmod_common/Spazzysmod/world/biome/BiomeMoonBiome.java
+++ Spazzysmod/spazzysmod_common/Spazzysmod/world/biome/BiomeMoonBiome.java
@@ -1,27 +1,21 @@
 package Spazzysmod.world.biome;
 
-import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.SpawnListEntry;
 import Spazzysmod.entity.passive.EntityGopher;
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class BiomeMoonBiome extends BiomeGenBase {
 
-    @SuppressWarnings("unchecked")
     public BiomeMoonBiome(int par1) {
         super(par1);
         this.setBiomeName("Moon Biome");
+        
         this.spawnableMonsterList.clear();
         this.spawnableCreatureList.clear();
         this.spawnableWaterCreatureList.clear();
         this.spawnableCaveCreatureList.clear();
-        this.spawnableCreatureList.add(new SpawnListEntry(EntityGopher.class,
-                5, 2, 4));
-        this.spawnableMonsterList.add(new SpawnListEntry(EntityEnderman.class,
-                10, 0, 2));
-        this.theBiomeDecorator.generateLakes = false;
-
-        // TODO Auto-generated constructor stub
+        
+        EntityRegistry.addSpawn(EntityGopher.class, 10, 1, 1, EnumCreatureType.creature, this);
     }
-
 }
